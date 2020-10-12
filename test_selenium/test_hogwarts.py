02904ee7c9d215882a442517898c2183973e3901
kuma-100 <787811198@qq.com>
2020-10-12 19:36:04 +0800

    add

--- test_selenium/test_hogwarts.py
+++ test_selenium/test_hogwarts.py
@@ -1,8 +1,18 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+
 
 class TestHogwarts:
     def setup_method(self):
         self.driver = webdriver.Edge()
         self.driver.get("https://testerhome.com/")
+
     def test_hogwarts(self):
-        pass
+        self.driver.find_element(By.LINK_TEXT,"社团").click()
+        self.driver.find_element(By.LINK_TEXT,"测试媛").click()
+        # sleep(2)
+        self.driver.find_element(By.CSS_SELECTOR,".topic-22425 .title > a").click()
+
+    def teardown_method(self):
+        # sleep(20)
+        self.driver.quit()
