a4863e4a230b4f325801eb584a59defa44437014
kuma-100 <787811198@qq.com>
2020-10-13 20:24:19 +0800

    add

--- test_selenium/test_hogwarts.py
+++ test_selenium/test_hogwarts.py
@@ -6,13 +6,17 @@ class TestHogwarts:
     def setup_method(self):
         self.driver = webdriver.Edge()
         self.driver.get("https://testerhome.com/")
+        self.driver.implicitly_wait(5)  # 隐式等待，找不到元素时重复查找直到5秒超时
 
     def test_hogwarts(self):
-        self.driver.find_element(By.LINK_TEXT,"社团").click()
-        self.driver.find_element(By.LINK_TEXT,"测试媛").click()
+        self.driver.find_element(By.LINK_TEXT, "社团").click()
+        # 尽量使用css的定位方法集，Link有可能会导致解析元素的时候出现异常
+        # todo：显式等待
+        self.driver.find_element(By.CSS_SELECTOR, "[data-name=测试媛]").click()
         # sleep(2)
-        self.driver.find_element(By.CSS_SELECTOR,".topic-22425 .title > a").click()
+        self.driver.find_element(By.CSS_SELECTOR, ".topic .title > a").click()
 
     def teardown_method(self):
+        pass
         # sleep(20)
-        self.driver.quit()
+        # self.driver.quit()
