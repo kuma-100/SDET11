594620cdf0b27a6f66ff2079ac6bb529e5272ea8
kuma-100 <787811198@qq.com>
2020-10-16 17:44:03 +0800

    add

--- test_selenium/test_hogwarts.py
+++ test_selenium/test_hogwarts.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
 
 
 class TestHogwarts:
@@ -12,6 +13,7 @@ class TestHogwarts:
         self.driver.find_element(By.LINK_TEXT, "社团").click()
         # 尽量使用css的定位方法集，Link有可能会导致解析元素的时候出现异常
         # todo：显式等待
+        WebDriverWait(self.driver,10).until()
         self.driver.find_element(By.CSS_SELECTOR, "[data-name=测试媛]").click()
         # sleep(2)
         self.driver.find_element(By.CSS_SELECTOR, ".topic .title > a").click()
