15e8e8246837e1862f63ada1d19c65f37ec72f5b
kuma-100 <787811198@qq.com>
2020-11-18 19:02:10 +0800

    添加login页面，隐藏内部元素

--- test_selenium/page/register.py
+++ test_selenium/page/register.py
@@ -6,5 +6,5 @@ from test_selenium.page.base_page import BasePage
 class Register(BasePage):
 
     def register(self, corpname):
-        self.driver.find_element(By.ID, "corp_name").send_keys(corpname)
-        self.driver.find_element(By.ID, "submit_btn").click()
+        self._driver.find_element(By.ID, "corp_name").send_keys(corpname)
+        self._driver.find_element(By.ID, "submit_btn").click()
--- test_selenium/testcase/test_index.py
+++ test_selenium/testcase/test_index.py
@@ -11,3 +11,9 @@ class TestIndex:
         self.index.goto_register().register("霍格沃兹学院")
         # self.driver.find_element(By.ID, "corp_name").send_keys("霍格沃兹学院")
         # self.driver.find_element(By.ID, "submit_btn").click()
+
+    def test_login(self):
+        self.index.goto_login().goto_registry().register("霍格沃兹学院")
+
+    def teardown(self):
+        self.index.close()
